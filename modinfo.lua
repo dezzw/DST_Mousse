443a753830624774c444cb4925ed64b53b2c5e4c
dezzw <dw@dezzw.com>
2024-05-06 23:55:00 -0400

    Init the mod info

---
+++ modinfo.lua
@@ -0,0 +1,24 @@
+-- base information
+name = "Mousse"
+description = "Custom character based on the Arknights's character Mousses."
+author = "DA"
+version = "0.1"
+
+forumthread = ""
+
+-- mod icons
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+-- mod settings
+-- [TODO] not sure the usage yet
+-- dont_starve_compatible = true
+-- reign_of_giants_compatible = true
+-- shipwrecked_compatible = true
+
+client_only_mod = true
+all_clients_require_mod = false
+api_version = 10
+
+-- possible configuration
+configuration_options = {}
